14745350ead99e598931ef4635a80ee81b94ce07
Axel Halifa <axel.halifa0@gmail.com>
2024-08-14 10:30:10 +0200

    Ajout : Mise en place des components Auth

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './features/auth/components/register/register.component';
+
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
